5e5da4be0c133d14ce7dc7b5cc31162267731211
Andrew Aghoghovwia <androyt1@gmail.com>
2024-04-09 15:45:04 +0100

    first

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
             <div className='flex-shrink-0'>
                 <h3 className='text-2xl font-normal text-neutral-200'>Androy</h3>
             </div>
-            <ul className='flex items-center justify-center text-neutral-400 gap-2'>
+            <ul className='hidden md:flex items-center justify-center text-neutral-400 gap-2'>
                 <li>About</li>
                 <li>Experience</li>
                 <li>Project</li>
