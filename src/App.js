1b173f4dae62a799bc3a893d2913842fac9c0b45
Andrew Aghoghovwia <androyt1@gmail.com>
2024-04-10 19:56:43 +0100

    dark mode

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
+import { useState, useEffect } from "react";
+import About from "./components/About";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import AnimatedCursor from "react-animated-cursor";
 
 export default function App() {
+    const [theme, setTheme] = useState("light");
+
+    const handleTheme = () => {
+        setTheme(theme === "dark" ? "light" : "dark");
+    };
+
+    useEffect(() => {
+        if (theme === "dark") {
+            document.documentElement.classList.add("dark");
+        } else {
+            document.documentElement.classList.remove("dark");
+        }
+    }, [theme]);
+
     return (
-        <div className=' [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]'>
-            {/* <div className='absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]'></div> */}
+        <div className='  relative'>
+            <div className='top-0 left-0 fixed inset-0 -z-10 h-full w-full items-center px-5 py-24 bg-white [background:radial-gradient(125%_125%_at_50%_10%,#fff_40%,#63e_100%)] dark:[background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]'></div>
             <div className='max-w-6xl mx-auto font-montserrat'>
-                <Navbar />
+                <Navbar theme={theme} handleTheme={handleTheme} />
                 <Hero />
+                <About />
             </div>
             <AnimatedCursor
                 innerSize={20}
